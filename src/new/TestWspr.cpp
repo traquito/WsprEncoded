d1ffcd8d1f20328ba3ca676f0fe9f78528477ecc
traquito <douglas.malnati+traquito@gmail.com>
2024-11-24 13:33:23 -0500

    TestWspr

--- src/new/TestWspr.cpp
+++ src/new/TestWspr.cpp
@@ -1,10 +1,99 @@
+#include <cstdint>
 #include <iostream>
+#include <vector>
 using namespace std;
 
+#include "TestUtl.h"
 #include "Wspr.h"
 
 
+#include "../WSPR.h"
+
+
+
+bool TestGetBandDataList()
+{
+    bool retVal = Wspr::GetBandDataList().size() != 0;
+
+    cout << "TestGetBandDataList test: " << retVal << endl;
+
+    return retVal;
+}
+
+bool TestGetDialFreqFromBandStr()
+{
+    static const uint32_t DIAL_FREQ_20M = 14095600;
+    static const uint32_t DIAL_FREQ_30M = 10138700;
+
+    vector<InputTest<const char *, uint32_t>> testList = {
+        { nullptr,  DIAL_FREQ_20M,  "nullptr",           },
+        { "",       DIAL_FREQ_20M,  "empty string",      },
+        { " ",      DIAL_FREQ_20M,  "space",             },
+        { "20m",    DIAL_FREQ_20M,  "valid 20m",         },
+        { "30m",    DIAL_FREQ_30M,  "valid 30m",         },
+        { "777m",   DIAL_FREQ_20M,  "non-existent 777m", },
+    };
+
+    bool retVal = true;
+    for (const auto &test : testList)
+    {
+        retVal &= CheckErr(test, Wspr::GetDialFreqFromBandStr(test.input));
+    }
+    for (const auto &test : testList)
+    {
+        // ignore null, string is known to not support this
+        if (test.input != nullptr)
+        {
+            retVal &= CheckErr(test, WSPR::GetDialFreqFromBandStr(test.input));
+        }
+    }
+
+    cout << "TestGetDialFreqFromBandStr test: " << retVal << endl;
+
+    return retVal;
+}
+
+bool TestGetPowerDbmList()
+{
+    bool retVal = Wspr::GetPowerDbmList().size() != 0;
+
+    cout << "TestGetPowerDbmList test: " << retVal << endl;
+
+    return retVal;
+}
+
+bool TestPowerDbmInSet()
+{
+    vector<InputTest<uint8_t, bool>> testList = {
+        { 0,  true,  "valid",   },
+        { 1,  false, "invalid", },
+        { 60, true,  "valid",   },
+        { 61, false, "invalid", },
+    };
+
+    bool retVal = true;
+    for (const auto &test : testList)
+    {
+        retVal &= CheckErr(test, Wspr::PowerDbmInSet(test.input));
+    }
+    for (const auto &test : testList)
+    {
+        retVal &= CheckErr(test, WSPR::PowerDbmInSet(test.input));
+    }
+
+    cout << "TestPowerDbmInSet test: " << retVal << endl;
+
+    return retVal;
+}
+
 int main(int argc, char *argv[])
 {
-    return 0;
+    bool retVal = true;
+
+    retVal &= TestGetBandDataList();
+    retVal &= TestGetDialFreqFromBandStr();
+    retVal &= TestGetPowerDbmList();
+    retVal &= TestPowerDbmInSet();
+
+    return !retVal;
 }
