bfb595673419fd51a74fb9989fab9ebe920459ea
traquito <douglas.malnati+traquito@gmail.com>
2024-12-05 01:23:05 -0500

    include binary just to see the size of UserDefined at different field capacities

---
+++ test/app/TestAppSizeUserDefined.cpp
@@ -0,0 +1,43 @@
+#include <cstdint>
+#include <iostream>
+using namespace std;
+
+#include "WsprEncoded.h"
+
+
+int main()
+{
+    cout << " 0, " << sizeof(WsprMessageTelemetryExtendedUserDefined<0>)  << endl;
+    cout << " 1, " << sizeof(WsprMessageTelemetryExtendedUserDefined<1>)  << endl;
+    cout << " 2, " << sizeof(WsprMessageTelemetryExtendedUserDefined<2>)  << endl;
+    cout << " 3, " << sizeof(WsprMessageTelemetryExtendedUserDefined<3>)  << endl;
+    cout << " 4, " << sizeof(WsprMessageTelemetryExtendedUserDefined<4>)  << endl;
+    cout << " 5, " << sizeof(WsprMessageTelemetryExtendedUserDefined<5>)  << endl;
+    cout << " 6, " << sizeof(WsprMessageTelemetryExtendedUserDefined<6>)  << endl;
+    cout << " 7, " << sizeof(WsprMessageTelemetryExtendedUserDefined<7>)  << endl;
+    cout << " 8, " << sizeof(WsprMessageTelemetryExtendedUserDefined<8>)  << endl;
+    cout << " 9, " << sizeof(WsprMessageTelemetryExtendedUserDefined<9>)  << endl;
+    cout << "10, " << sizeof(WsprMessageTelemetryExtendedUserDefined<10>) << endl;
+    cout << "11, " << sizeof(WsprMessageTelemetryExtendedUserDefined<11>) << endl;
+    cout << "12, " << sizeof(WsprMessageTelemetryExtendedUserDefined<12>) << endl;
+    cout << "13, " << sizeof(WsprMessageTelemetryExtendedUserDefined<13>) << endl;
+    cout << "14, " << sizeof(WsprMessageTelemetryExtendedUserDefined<14>) << endl;
+    cout << "15, " << sizeof(WsprMessageTelemetryExtendedUserDefined<15>) << endl;
+    cout << "16, " << sizeof(WsprMessageTelemetryExtendedUserDefined<16>) << endl;
+    cout << "17, " << sizeof(WsprMessageTelemetryExtendedUserDefined<17>) << endl;
+    cout << "18, " << sizeof(WsprMessageTelemetryExtendedUserDefined<18>) << endl;
+    cout << "19, " << sizeof(WsprMessageTelemetryExtendedUserDefined<19>) << endl;
+    cout << "20, " << sizeof(WsprMessageTelemetryExtendedUserDefined<20>) << endl;
+    cout << "21, " << sizeof(WsprMessageTelemetryExtendedUserDefined<21>) << endl;
+    cout << "22, " << sizeof(WsprMessageTelemetryExtendedUserDefined<22>) << endl;
+    cout << "23, " << sizeof(WsprMessageTelemetryExtendedUserDefined<23>) << endl;
+    cout << "24, " << sizeof(WsprMessageTelemetryExtendedUserDefined<24>) << endl;
+    cout << "25, " << sizeof(WsprMessageTelemetryExtendedUserDefined<25>) << endl;
+    cout << "26, " << sizeof(WsprMessageTelemetryExtendedUserDefined<26>) << endl;
+    cout << "27, " << sizeof(WsprMessageTelemetryExtendedUserDefined<27>) << endl;
+    cout << "28, " << sizeof(WsprMessageTelemetryExtendedUserDefined<28>) << endl;
+    cout << "29, " << sizeof(WsprMessageTelemetryExtendedUserDefined<29>) << endl;
+    cout << "30, " << sizeof(WsprMessageTelemetryExtendedUserDefined<30>) << endl;
+
+    return 0;
+}
